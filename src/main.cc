e606dab0c32505fdd5bd20d98cc28c83e5ed71c9
Hajime Fukuda <hajime.fukuda@me.com>
2017-10-31 18:30:57 +0900

    changed to the soft QCD

--- src/main.cc
+++ src/main.cc
@@ -7,25 +7,21 @@ using Pythia8::Hist;
 int main() {
   Pythia pythia;
   pythia.readString("Beams:eCM = 13000.");
-  pythia.readString("HardQCD:all = on");
+  pythia.readString("SoftQCD:all = on");
   pythia.readString("PhaseSpace:pTHatMin = 200.");
   pythia.init();
 
   Hist mult("charged multiplicity", 100, -0.5, 799.5);
 
-  // Begin event loop. Generate event. Skip if error. List first one.
   for (int iEvent = 0; iEvent < 1; ++iEvent) {
     if (!pythia.next()) continue;
 
-    // Find number of all final charged particles and fill histogram.
     int nCharged = 0;
     for (int i = 0; i < pythia.event.size(); ++i) {
       if (pythia.event[i].isFinal() && pythia.event[i].isHadron()) {
         std::cout << pythia.event[i].pT() << std::endl;
       }
     }
-    // mult.fill( nCharged );
-    // End of event loop. Statistics. Histogram. Done.
   }
   pythia.stat();
 
