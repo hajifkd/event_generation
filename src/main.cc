9543be82aa3bdcfed7f04a01a1b021d8a052e796
Hajime Fukuda <hajime.fukuda@me.com>
2017-10-31 19:30:53 +0900

    write pt/pz

--- src/main.cc
+++ src/main.cc
@@ -2,28 +2,28 @@
 #include "Pythia8/Pythia.h"
 
 using Pythia8::Pythia;
-using Pythia8::Hist;
 
 int main() {
   Pythia pythia;
-  pythia.readString("Beams:eCM = 13000.");
+  pythia.readString("Beams:eCM = 14000.");
   pythia.readString("SoftQCD:all = on");
-  pythia.readString("PhaseSpace:pTHatMin = 200.");
   pythia.init();
-
-  Hist mult("charged multiplicity", 100, -0.5, 799.5);
 
   for (int iEvent = 0; iEvent < 1; ++iEvent) {
     if (!pythia.next()) continue;
 
-    int nCharged = 0;
-    for (int i = 0; i < pythia.event.size(); ++i) {
+    std::cerr << pythia.event.size() << std::endl;
+
+    for (int i = 0; i < pythia.event.size(); i++) {
       if (pythia.event[i].isFinal() && pythia.event[i].isHadron()) {
-        std::cout << pythia.event[i].pT() << std::endl;
+        // Maybe we should reject too High pz, otherwise it would be the end-cap region
+        std::cerr << pythia.event[i].pT() << "," << pythia.event[i].pz() << std::endl;
       }
     }
+
+    // We need information for the number of the jets in one vertex, maybe
+    std::cerr << std::endl;
   }
-  pythia.stat();
 
   return 0;
 }
